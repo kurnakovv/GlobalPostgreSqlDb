c38ace13ffaf78a21fd17142bb83c1e6fc8235d2
KurnakovMaksim <maksk210@gmail.com>
2023-03-28 22:25:17 +0900

    Add project files.

---
+++ GlobalDB/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GlobalDB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public UserController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_appDbContext.Users.ToList());
+        }
+    }
+}
---
+++ GlobalDB/UserEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GlobalDB
+{
+    [Table("user")]
+    public class UserEntity
+    {
+        [Column("id_user")]
+        public long Id { get; set; }
+
+        [Column("u_name")]
+        public string Name { get; set; }
+    }
+}
